1340324093b1d0cc81e87615eb3a6af88c89460c
Neemias Vieira <neemias.vieira@live.com>
2023-08-23 16:35:04 -0300

    Iniciando deploy na vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { CarsModule } from './modules/cars/cars.module';
 import { ClientsModule } from './modules/clients/clients.module';
 import { AdminsModule } from './modules/admins/admins.module';
 import { ReservesModule } from './modules/reserves/reserves.module';
-import { LoginController } from './clients/use-cases/login/login.controller';
+
 
 @Module({
   imports: [DatabaseModule, CarsModule, ClientsModule, AdminsModule, CarsModule, ReservesModule],
-  controllers: [AppController, LoginController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/modules/clients/use-cases/login/login.controller.ts
+++ src/modules/clients/use-cases/login/login.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { LoginService } from './login.service';
 
 @Controller('login')
-export class LoginController {}
+export class LoginController {
+    constructor(private readonly appservice: LoginService){}
+
+@Post()
+async login(@Body() body : {email: string, password: string}) {
+    const {email, password} = body;
+    return await this.appservice.login(email, password);
+    
+}
+
+}
