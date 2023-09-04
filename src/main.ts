955fe2b355d53b42b6db83c7089dce93fb606271
Neemias Vieira <neemias.vieira@live.com>
2023-09-04 14:07:32 -0300

    Title was changed

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document, {
-    customSiteTitle: 'Sistema canoa',
+    customSiteTitle: 'DriveExperience - API Documentation',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
